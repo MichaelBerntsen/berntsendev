aea689d362bdf2b03d31711a1cbcb827411afa90
Michael Berntsen <mb@zalt.no>
2023-08-01 23:02:15 +0200

    v0.1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,11 +14,11 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
+          <div className={styles.buttons}>
+        <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Tutorials - 5min ⏱️
+            All Microsoft Cloud ☁️🚀 (and other stuff too...) 
           </Link>
         </div>
       </div>
